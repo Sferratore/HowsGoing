f033d1096eb5e520312179dced5415a208e4f7d7
Sferratore <sferratore.francescopaolo00@gmail.com>
2022-12-15 22:57:53 +0100

    added homepage and button 'post'

--- HowsGoing/Controllers/HomeController.cs
+++ HowsGoing/Controllers/HomeController.cs
@@ -27,6 +27,11 @@ namespace HowsGoing.Controllers
             return View();
         }
 
+        public IActionResult Postpage()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
